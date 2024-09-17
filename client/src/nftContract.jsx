c99ae15ea249c4430cf595b30bbc44c13437276c
ysaliji023 <ysaliji023@student.wethinkcode.co.za>
2024-09-17 14:14:56 +0200

    created file

--- client/src/nftContract.jsx
+++ client/src/nftContract.jsx
@@ -5,7 +5,8 @@ const nftContract = web3 => {
     return new web3.eth.Contract(
         mintContract,  
         // "0xA9aae2CDae58fef266991eD3fFaB7F6C7eDC6A18"
-        "0xd37FCC24689BFDd0722A580AA49EF76BaAaF13E0"
+        // "0xd37FCC24689BFDd0722A580AA49EF76BaAaF13E0"
+        "0x2e8021aa783cA1f755597103C3066BD36E6E084a"
     )
 }
 
