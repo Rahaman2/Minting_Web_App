0a08fa5806c48985c630ee9acb3801510748e693
Rahaman <rahamankazembe@gmail.com>
2024-09-28 14:37:00 +0200

    connected to backend unsuccesful

--- client/src/nftContract.jsx
+++ client/src/nftContract.jsx
@@ -1,12 +1,10 @@
 import Web3 from 'web3';
-import mintContract from './contracts/mohtore.json'; // Import the JSON file of your contract
+import mintContract from './contracts/raha.json'; // Import the JSON file of your contract
 
 const nftContract = web3 => {
     return new web3.eth.Contract(
         mintContract,  
-        // "0xA9aae2CDae58fef266991eD3fFaB7F6C7eDC6A18"
-        // "0xd37FCC24689BFDd0722A580AA49EF76BaAaF13E0"
-        "0x2e8021aa783cA1f755597103C3066BD36E6E084a"
+        "0x07966752b3b4716DaC0f7D28Fef5C87213baC2B5"
     )
 }
 
